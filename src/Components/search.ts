116a2dde01e31f37198ec3a7d1f275ef13c6baae
qihyang <qihyang@ebay.com>
2020-08-07 13:59:12 +0800

    Add new request util to support proxy

--- src/Components/search.ts
+++ src/Components/search.ts
@@ -1,16 +1,10 @@
-import axio from 'axios'
 import { RequestHandler } from 'express-serve-static-core'
+import { request } from '../Utils/request'
 
 const Search : RequestHandler = async (req,res) => {
     const keywords = decodeURIComponent(req.query.keywords);
     try {
-        res.send((await axio.get('http://songsearch.kugou.com/song_search_v2',{
-            params:{
-                keyword : keywords,
-                page : 1,
-                pagesize : 30
-            }
-        })).data);
+        res.send(await request(`http://songsearch.kugou.com/song_search_v2?keyword=${encodeURIComponent(keywords)}&page=1&pagesize=30`, req.query.proxy));
     }
     catch(err) {
         res.status(502).send(err);
--- src/Components/songurl.ts
+++ src/Components/songurl.ts
@@ -1,18 +1,12 @@
-import axio from 'axios'
 import { RequestHandler } from 'express-serve-static-core'
 import { KugouMid } from '../Utils/encryptor'
+import { request } from '../Utils/request';
 
 const GetSongUrl : RequestHandler = async (req,res) => {
     const hash = decodeURIComponent(req.query.hash).toUpperCase();
     const mid = KugouMid();
     try {
-        res.send((await axio.get('https://wwwapi.kugou.com/yy/index.php',{
-            params:{
-                r : 'play/getdata',
-                hash: hash,
-                mid: mid
-            }
-        })).data);
+        res.send(await request(`http://wwwapi.kugou.com/yy/index.php?r=play/getdata&hash=${hash}&mid=${mid}`, req.query.proxy));
     }
     catch(err) {
         res.status(502).send(err);
